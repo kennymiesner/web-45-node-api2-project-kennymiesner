e83b81e88507160aa4b2e67551190ac4181dc7a5
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-31 16:20:25 -0500

    Adds PUT request for updated post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -53,7 +53,6 @@ router.post('/', (req, res) => {
         res.status(201).json(post)
       })
       .catch(err => {
-        console.log(err)
         res.status(500).json({
           message: "There was an error while saving the post to the database",
           err: err.message,
@@ -61,6 +60,41 @@ router.post('/', (req, res) => {
         })
       })
   }
+})
+
+router.put('/:id', (req, res) => {
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  } else {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (!post) {
+          res.status(404).json({
+            message: 'The post with the specified ID does not exist',
+          })
+        } else {
+          return Post.update(req.params.id, req.body)
+        }
+      })
+      .then(data => {
+        if (data) {
+          return Post.findById(req.params.id)
+        }
+      })
+      .then(post => {
+        res.json(post)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+    }
 })
 
 module.exports = router
