4f467817e3b7e4a781af139bd4d341c062dbde35
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-31 15:52:55 -0500

    Adds POST request for new post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -10,7 +10,9 @@ router.get('/', (req, res) => {
     .catch(err => {
       console.log(err)
       res.status(500).json({
-        message: "The posts information could not be retrieved"
+        message: "The posts information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
       })
     })
 })
@@ -29,9 +31,36 @@ router.get('/:id', (req, res) => {
     .catch(err => {
       console.log(err)
       res.status(500).json({
-        message: "The post information could not be retrieved"
+        message: "The post information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
       })
     })
+})
+
+router.post('/', (req, res) => {
+  const { title, contents } = req.body
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  } else {
+    Post.insert({ title, contents })
+      .then(({ id }) => {
+        return Post.findById(id)
+      })
+      .then(post => {
+        res.status(201).json(post)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+          err: err.message,
+          stack: err.stack,
+        })
+      })
+  }
 })
 
 module.exports = router
