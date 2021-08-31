94dd61ddb7b0d44b6888903a232b5e5886d0cfb7
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-31 16:34:20 -0500

    Adds DELETE request for post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -66,14 +66,14 @@ router.put('/:id', (req, res) => {
   const { title, contents } = req.body
   if (!title || !contents) {
     res.status(400).json({
-      message: "Please provide title and contents for the post"
+      message: 'Please provide title and contents for the post'
     })
   } else {
     Post.findById(req.params.id)
       .then(post => {
         if (!post) {
           res.status(404).json({
-            message: 'The post with the specified ID does not exist',
+            message: 'The post with the specified ID does not exist'
           })
         } else {
           return Post.update(req.params.id, req.body)
@@ -89,12 +89,32 @@ router.put('/:id', (req, res) => {
       })
       .catch(err => {
         res.status(500).json({
-          message: "There was an error while saving the post to the database",
+          message: 'There was an error while saving the post to the database',
           err: err.message,
           stack: err.stack,
         })
       })
     }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+      res.status(404).json({
+        message: 'The post with the specified ID does not exist'
+      })
+    } else {
+      await Post.remove(req.params.id)
+      res.json(post)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The post could not be removed',
+      err: err.message,
+      stack: err.stack,
+    })
+  }
 })
 
 module.exports = router
